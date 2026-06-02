3594f39df7b3afb53902225789c840b69f40d0d2
yama-2561 <yamakenty510@gmail.com>
2026-06-02 23:36:52 +0900

    Update capital.js

--- js/capital.js
+++ js/capital.js
@@ -1,17 +1,17 @@
 function viewChange(){
     if(document.getElementById('tgroup')){
         id = document.getElementById('tgroup').value;
+          document.getElementById('2502').style.display = "none";
+          document.getElementById('2603').style.display = "none";
+          document.getElementById('2605').style.display = "none";
         if(id == '202502'){
             document.getElementById('2502').style.display = "";
-            document.getElementById('2603').style.display = "none";
         }
         else if(id == '202603'){
-            document.getElementById('2502').style.display = "none";
             document.getElementById('2603').style.display = "";
         }
-        else if(id == '000000'){
-            document.getElementById('2502').style.display = "none";
-            document.getElementById('2603').style.display = "none";
+        else if(id == '202605'){
+            document.getElementById('2605').style.display = "";
         }
     }
 
